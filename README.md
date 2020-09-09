5d4cb99e6f2de9c4279b2cfe15d49917266297a5
Lucas Soares <lsoareshfb@gmail.com>
2020-09-09 11:07:13 -0300

    Cadastro Finalizado

--- README.md
+++ README.md
@@ -3,5 +3,10 @@
 Desenvolvido no Bootcamp da Rocketseat, o GoBarber é uma aplicação de agendamentos de serviços de barbearia, prestados pelos usuários na aplicação.
 <p></p>
 Utilizamos o React Native para a criação da aplicação.
+
+---
+<video autoplay>
+    <source src="video.mov" type="video/mp4">
+</vide>
 <p></p>
 Criado por Lucas Soares
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,24 +1,49 @@
 import React from 'react';
-import {Image} from 'react-native';
+import {Image, KeyboardAvoidingView, View, ScrollView} from 'react-native';
 
 import logoImg from '../../assets/logo.png';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-import {Container, Title } from './styles';
+import {Container, Title, ForgotPassword, ForgotPasswordText, CreateAccountButton, CreateAccountButtonText, Icon} from './styles';
 
 const SignIn: React.FC = () => {
     return (
-        <Container>
-            <Image source={logoImg} />
-            <Title>Faça seu logon</Title>
+        <>
+            <KeyboardAvoidingView
+                style={{flex:1}}
+                behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+                enabled
+            >
+                <ScrollView
+                keyboardShouldPersistTaps="handled"
+                contentContainerStyle={{flex: 1}}
+                >
+                    <Container>
+                        <Image source={logoImg} />
 
-            <Input name="email" icon="mail" placeholder="E-mail"/>
-            <Input name="password" icon="lock" placeholder="Senha"/>
+                        <View>
+                            <Title>Faça seu logon</Title>
+                        </View>
+                        <Input name="email" icon="mail" placeholder="E-mail"/>
+                        <Input name="password" icon="lock" placeholder="Senha"/>
 
-            <Button onPress={() => {}}>Entrar</Button>
-        </Container>
+                        <Button onPress={() => {}}>Entrar</Button>
+
+                        <ForgotPassword onPress={() => {}}>
+                            <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+                        </ForgotPassword>
+                    </Container>
+                </ScrollView>
+            </KeyboardAvoidingView>
+
+            <CreateAccountButton onPress={() => {}}>
+                <Icon name="log-in" size={20} color="#ff9000" />
+                <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+            </CreateAccountButton>
+        </>
+
     )
 }
 
